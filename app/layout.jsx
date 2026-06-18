1e8c5d4b3ca54053fc6ca17c8259d9c66a203c2a
dydevx <laitanduylqd02@gmail.com>
2026-06-18 16:15:02 +0700

    upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="vi">
+    <html lang="vi" suppressHydrationWarning>
       <body>
         <SiteHeader />
         <main>{children}</main>
